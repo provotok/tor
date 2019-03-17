5e964ff2fcf4366103b44d0160866d37068dd2a5
provotok <48186925+provotok@users.noreply.github.com>
2019-03-17 09:02:45 +0300

    Update eazy_TOR_ssh.sh

--- eazy_TOR_ssh.sh
+++ eazy_TOR_ssh.sh
@@ -1,4 +1,4 @@
-sudo apt install tor
-sudo echo -e "HiddenServiceDir /var/lib/tor/ssh\nHiddenServicePort 22 127.0.0.1:22"
-sudo /etc/init.d/tor start
-sudo cat /var/lib/tor/ssh/hostname
+apt install tor
+echo -e "HiddenServiceDir /var/lib/tor/ssh\nHiddenServicePort 22 127.0.0.1:22"
+/etc/init.d/tor start
+cat /var/lib/tor/ssh/hostname
